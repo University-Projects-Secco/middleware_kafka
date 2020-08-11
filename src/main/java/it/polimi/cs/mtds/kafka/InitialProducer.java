85259ded561a4de7a90e0da805b3424895c3bc9c
Galaxar <matteo.secco97@gmail.com>
2020-08-11 18:42:11 +0200

    Add InitialProducer to feed the pipeline at the beginning

---
+++ src/main/java/it/polimi/cs/mtds/kafka/InitialProducer.java
@@ -0,0 +1,38 @@
+package it.polimi.cs.mtds.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+
+public class InitialProducer {
+
+	public static void main(String[] args) throws IOException {
+		final Properties properties = new Properties();
+		properties.put("bootstrap.servers","localhost:9092");
+		properties.put("key.serializer", StringSerializer.class);
+		properties.put("key.deserializer", StringDeserializer.class);
+		properties.put("value.serializer", StringSerializer.class);
+		properties.put("value.deserializer", StringDeserializer.class);
+		final KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+		final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		final DateTimeFormatter clock = DateTimeFormatter.ofPattern("HH:mm:ss");
+		int key=0;
+		String cmd;
+		do{
+			cmd=in.readLine();
+			final ProducerRecord<String,String> record = new ProducerRecord<>("topic_1", String.valueOf(key), cmd);
+			key++;
+			producer.send(record);
+			System.out.println(clock.format(LocalDateTime.now())+": sent '"+cmd+"'. type 'end' to quit");
+		}while ( !"end".equals(cmd) );
+	}
+	
+}
