4714cc9042e0470b2a72699c5cbe68ec9f2b462e
Galaxar <matteo.secco97@gmail.com>
2020-08-17 18:30:55 +0200

    Add Constants.java

---
+++ src/main/java/it/polimi/cs/mtds/kafka/constants/Constants.java
@@ -0,0 +1,9 @@
+package it.polimi.cs.mtds.kafka.constants;
+
+public final class Constants {
+	private Constants(){}
+	public static final String TOPIC_PREFIX = "topic_";
+	public static final String CONSUMER_GROUP_PREFIX = "consumer-";
+	public static final String PRODUCER_GROUP_PREFIX = "producer-";
+	public static final String STATE_GROUP_PREFIX = "states-";
+}
