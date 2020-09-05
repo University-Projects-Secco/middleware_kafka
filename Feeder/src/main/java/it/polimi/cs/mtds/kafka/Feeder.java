d3845513376784ced292989cad9355593c32af22
Galaxar <matteo.secco97@gmail.com>
2020-09-05 21:52:39 +0200

    Extended Feeder for the demo

--- Feeder/src/main/java/it/polimi/cs/mtds/kafka/Feeder.java
+++ Feeder/src/main/java/it/polimi/cs/mtds/kafka/Feeder.java
@@ -14,7 +14,14 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Properties;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.Thread.sleep;
+
 public class Feeder {
+
+	private static final DateTimeFormatter clock = DateTimeFormatter.ofPattern("HH:mm:ss");
+	private static KafkaProducer<String,String> producer;
+	private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
 	public static void main(String[] args) throws IOException {
 		final Properties properties = new Properties();
@@ -27,18 +34,52 @@ public class Feeder {
 		properties.put("key.deserializer", StringDeserializer.class);
 		properties.put("value.serializer", StringSerializer.class);
 		properties.put("value.deserializer", StringDeserializer.class);
-		final KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
-		final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		final DateTimeFormatter clock = DateTimeFormatter.ofPattern("HH:mm:ss");
+		producer = new KafkaProducer<>(properties);
+		help();
 		int key=0;
 		String cmd;
 		do{
 			cmd=in.readLine();
-			final ProducerRecord<String,String> record = new ProducerRecord<>("topic_1", String.valueOf(key), cmd);
-			key++;
-			producer.send(record);
-			System.out.println(clock.format(LocalDateTime.now())+": sent '"+cmd+"'. type 'end' to quit");
+			if("help".equalsIgnoreCase(cmd)||"h".equalsIgnoreCase(cmd)){
+				help();
+				continue;
+			}
+			try{
+				if(cmd.startsWith("auto")){
+					final String[] splitted = cmd.split("\\s|,|\\.|-|_");
+					final int messages = parseInt(splitted[1]);
+					final int bulkSize = splitted.length>=3? parseInt(splitted[2]):Integer.MAX_VALUE;
+					for ( int i = 0,j=1; i < messages; i++, j++ ) {
+						sendMessage(key++, key);
+						if(j%bulkSize==0) {
+							System.out.println("waiting 5 seconds");
+							sleep(5000);
+						}
+					}
+				}else {
+					final int val = parseInt(cmd);
+					sendMessage(key++,val);
+				}
+			}catch ( NumberFormatException | ArrayIndexOutOfBoundsException e ){
+				System.out.println("Please insert integer numbers, 'auto %d', or 'end'");
+			} catch ( InterruptedException e ) {
+				e.printStackTrace();
+			}
 		}while ( !"end".equals(cmd) );
 	}
-	
+
+	private static void sendMessage(int key, int value){
+		final ProducerRecord<String, String> record = new ProducerRecord<>("topic_1", String.valueOf(key), String.valueOf(value));
+		producer.send(record);
+		System.out.println(clock.format(LocalDateTime.now()) + ": sent '" + value + "'. type 'end' to quit");
+	}
+
+	private static void help(){
+		System.out.println("\n\nUsage:\n" +
+				"%d sends the value %d\n" +
+				"auto-%d1[-%d2] sends %d1 messages automatically generated, waiting 5 seconds every %d2 messages\n" +
+				"end quits\n" +
+				"help|h shows this message\n\n");
+	}
+
 }
