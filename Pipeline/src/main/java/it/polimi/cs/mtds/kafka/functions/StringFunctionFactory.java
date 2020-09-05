3f7729d73004df8e0bdd444c4c08130a409f033f
Galaxar <matteo.secco97@gmail.com>
2020-09-05 21:15:49 +0200

    Logging functions

--- Pipeline/src/main/java/it/polimi/cs/mtds/kafka/functions/StringFunctionFactory.java
+++ Pipeline/src/main/java/it/polimi/cs/mtds/kafka/functions/StringFunctionFactory.java
@@ -2,8 +2,15 @@ package it.polimi.cs.mtds.kafka.functions;
 
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
+import java.util.logging.Logger;
 
 public final class StringFunctionFactory implements FunctionFactory<String,String,String> {
+	
+	private static final Logger logger = Logger.getLogger(StringFunctionFactory.class.getName());
+	
+	private static String getOperationInfo(String string, String state, String result, String operation){
+		return "Executed "+operation+" on ("+string+","+state+") with result "+result;
+	}
 
 	/**
 	 * Builds the predefined functions. Lambda can be replaced with custom functional interfaces ({@link ExampleExternalFunction})
@@ -13,15 +20,35 @@ public final class StringFunctionFactory implements FunctionFactory<String,Strin
 	public BiFunction<String, AtomicReference<String>, String> getFunction(String name){
 		switch ( name.toLowerCase() ){
 			case "int:increment":
-				return (string,state)->String.valueOf(Integer.parseInt(string)+1);
+				return (string,state)->{
+					final String result = String.valueOf(Integer.parseInt(string)+1);
+					logger.info(getOperationInfo(string,state.get(),result, name.toLowerCase()));
+					return result;
+				};
 			case "int:decrement":
-				return (string,state)->String.valueOf(Integer.parseInt(string)-1);
+				return (string,state)->{
+					final String result = String.valueOf(Integer.parseInt(string)-1);
+					logger.info(getOperationInfo(string,state.get(),result, name.toLowerCase()));
+					return result;
+				};
 			case "int:double":
-				return (string,state)->String.valueOf(Integer.parseInt(string)*2);
+				return (string,state)->{
+					final String result = String.valueOf(Integer.parseInt(string)*2);
+					logger.info(getOperationInfo(string,state.get(),result, name.toLowerCase()));
+					return result;
+				};
 			case "int:half":
-				return (string,state)->String.valueOf(Integer.parseInt(string)/2);
+				return (string,state)->{
+					final String result = String.valueOf(Integer.parseInt(string)/2);
+					logger.info(getOperationInfo(string,state.get(),result, name.toLowerCase()));
+					return result;
+				};
 			case "int:add_state":
-				return (string, state)->String.valueOf(Integer.parseInt(string)+Integer.parseInt(state.getAndAccumulate("1",(st,inc)-> String.valueOf(Integer.parseInt(st)+Integer.parseInt(inc)))+" "+string));
+				return (string, state)->{
+					final String result = String.valueOf(Integer.parseInt(string)+Integer.parseInt(state.getAndAccumulate("1",(st,inc)-> String.valueOf(Integer.parseInt(st)+Integer.parseInt(inc)))+" "+string));
+					logger.info(getOperationInfo(string,state.get(),result, name.toLowerCase()));
+					return result;
+				};
 			case "string:print":
 				return (string,state)->{
 					System.out.println(state.getAndAccumulate("1",(st,inc)-> String.valueOf(Integer.parseInt(st)+Integer.parseInt(inc)))+" "+string);
@@ -30,7 +57,10 @@ public final class StringFunctionFactory implements FunctionFactory<String,Strin
 			case "string:shuffle":
 				return new ExampleExternalFunction();
 			default:
-				return (string,state)->string;
+				return (string,state)->{
+					logger.info(getOperationInfo(string,state.get(),string, name.toLowerCase()));
+					return string;
+				};
 		}
 	}
 }
