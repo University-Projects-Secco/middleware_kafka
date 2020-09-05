ee35014eb319ec70147b32e7cd6b8566b774dee1
Galaxar <matteo.secco97@gmail.com>
2020-09-05 17:45:24 +0200

    Improved cleanup, removed '-' from container names in brokers and clients

--- docker/scripts/run_brokers.sh
+++ docker/scripts/run_brokers.sh
@@ -1,6 +1,5 @@
 NUMBER=${1-2}
 KAFKA_PATH=${2-'/home/matteo/University/Middleware_technologies_for_distributed_systems/kafka_2.12-2.3.1'}
-PROPERTIES=${3-"$KAFKA_PATH/config/server.properties"}
 DIRECTORY="../Broker"
 
 cp -r "$KAFKA_PATH" "$DIRECTORY/kafka"
@@ -9,7 +8,8 @@ for (( i = 1; i <= NUMBER; i++ )); do
   cp "$DIRECTORY/server_template.properties" "$DIRECTORY/server.properties"
   printf 'broker.id=%d' "$i">>"$DIRECTORY/server.properties"
   docker build -t "kafka/broker$i" "$DIRECTORY"
-  docker run -d --network 'kafka-net' --ip "broker-$i" --name "broker-$i" "kafka/broker$i"
+  docker run -d --rm --network 'kafka-net' --name "broker$i" "kafka/broker$i"
 done
 
-rm -r "$DIRECTORY/kafka"
+rm -R "$DIRECTORY/kafka" &
+rm "$DIRECTORY/server.properties" &
--- docker/scripts/run_clients.sh
+++ docker/scripts/run_clients.sh
@@ -4,12 +4,11 @@ for properties in ./*.properties; do
   if [ -f "$properties" ]; then
     fileName=$(echo "$properties" | cut -f 2 -d '/')
     machineName=$(echo "$fileName" | cut -f 1 -d '.')
-    machineNum=$(echo "$machineName" | cut -f 2 -d '-')
     directory="../$machineName"
-    rm -r "$directory"
-    cp -r "../Pipeline" "$directory"
-    cp "$properties" "$directory/config.properties"
+    cp -R "../Pipeline" "$directory"
+    mv "$properties" "$directory/config.properties"
     docker build -t "kafka/$machineName" "$directory"
-    docker run -d --network 'kafka-net' --ip "client-$machineNum" --name "$machineName" "kafka/$machineName"
+    docker run -d --rm --network 'kafka-net' --name "$machineName" "kafka/$machineName"
+    rm -R "$directory" &
   fi;
 done
